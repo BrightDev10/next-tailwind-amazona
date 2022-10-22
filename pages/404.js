9d8243da9f681144e17e909daba4b83d184c3937
brightdev <brightdev10@gmail.coom>
2022-10-22 13:42:21 +0100

    added-products-page-edited-global-css-file

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function errorPage() {
+  return (
+    <div title="404-error-page" className='min-h-screen text-center items-center bg-slate-300'>
+      This page can not be found! 😡
+    </div>
+  )
+}
---
+++ utils/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [
+        {
+            name: 'Slim shirt',
+            slug: 'slim-shirt',
+            category: 'Shirts',
+            image: 'https://images.unsplash.com/photo-1596755094514-f87e34085b2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 3,
+            countInstock: 20,
+            description: 'A popular shirt'
+        },
+        {
+            name: 'Fit shirt',
+            slug: 'fit-shirt',
+            category: 'Shirts',
+            image: 'https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+            price: 100,
+            brand: 'Raymond',
+            rating: 3.5,
+            numReviews: 6,
+            countInstock: 80,
+            description: 'A popular fit shirt'
+        },
+        {
+            name: 'Casual shirt',
+            slug: 'casual-shirt',
+            category: 'Shirts',
+            image: 'https://images.unsplash.com/photo-1578587018452-892bacefd3f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+            price: 1000,
+            brand: 'Raymond',
+            rating: 3.5,
+            numReviews: 12,
+            countInstock: 30,
+            description: 'A popular casual shirt'
+        },
+        {
+            name: 'Corpoarate pants',
+            slug: 'corporate-pants',
+            category: 'Pants',
+            image: 'https://images.unsplash.com/photo-1584865288642-42078afe6942?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cGFudHN8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+            price: 3000,
+            brand: 'Versace',
+            rating: 4.5,
+            numReviews: 22,
+            countInstock: 10,
+            description: 'A popular corporate pant'
+        },
+        {
+            name: 'Casual pants',
+            slug: 'casual-pants',
+            category: 'Pants',
+            image: 'https://images.unsplash.com/photo-1594633312681-425c7b97ccd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFudHN8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+            price: 1000,
+            brand: 'Versace',
+            rating: 5,
+            numReviews: 12,
+            countInstock: 30,
+            description: 'A popular casual pant'
+        },
+        {
+            name: 'Golf pants',
+            slug: 'golf-pants',
+            category: 'Pants',
+            image: 'https://images.unsplash.com/photo-1541099649105-f69ad21f3246?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGFudHN8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60',
+            price: 1000,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 22,
+            countInstock: 10,
+            description: 'A popular golf pant'
+        },
+    ]
+}
+ export default data;
